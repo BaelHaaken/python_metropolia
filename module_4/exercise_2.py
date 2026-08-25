dab8a914fc6d816e5933db9e0fe66bc574be9117
Bael Haaken <hoangphale12345@gmail.com>
2026-08-25 14:10:53 +0300

    fix ex 2

--- module_4/exercise_2.py
+++ module_4/exercise_2.py
@@ -1,19 +1,11 @@
 cabin_class = str(input("Enter the cabin class: "))
-if cabin_class == "LUX":
+if cabin_class == "LUX" :
     print("The cabin class is LUX: upper-deck cabin with a balcony..")
-elif cabin_class == "A":
+elif cabin_class == "A" :
     print("The cabin class is A: above the car deck, equipped with a window.")
-elif cabin_class == "B":
+elif cabin_class == "B" :
     print("The cabin class is B: windowless cabin above the car deck.")
-elif cabin_class == "C":
-    print("The cabin class is C: windowless cabin below the car deck.")
-elif cabin_class == "lux":
-    print("The cabin class is LUX: upper-deck cabin with a balcony.")
-elif cabin_class == "a":
-    print("The cabin class is A: above the car deck, equipped with a window.")
-elif cabin_class == "b":
-    print("The cabin class is B: windowless cabin above the car deck.")
-elif cabin_class == "c":
+elif cabin_class == "C" :
     print("The cabin class is C: windowless cabin below the car deck.")
 else:
     print("Invalid cabin class.")
