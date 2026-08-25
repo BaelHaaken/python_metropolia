9252c44535984056dc9e16d5c0a85c601157cfa2
Bael Haaken <hoangphale12345@gmail.com>
2026-08-25 13:31:12 +0300

    fix ex 1

--- module_4/exercise_1.py
+++ module_4/exercise_1.py
@@ -1,5 +1,5 @@
-zander_size = float(input("Enter the size of the zander (in cm): "))
+zander_size = float(input("Enter the length of the zander (in cm): "))
 if zander_size < 42:
-    print(f"The zander is {42 - zander_size} cm below the size limit.")
+    print(f"The zander is {42 - zander_size} cm below the length limit.")
 else:
-    print("The zander meets the size limit.")
+    print("The zander meets the length limit.")
