eb79fc779c0308b64a379cc668006b89887bd53a
Bael Haaken <hoangphale12345@gmail.com>
2026-08-25 14:51:08 +0300

    nth changed

--- module_4/exercise_3.py
+++ module_4/exercise_3.py
@@ -1,14 +1,14 @@
 biological_gender = str(input("Enter biological gender (male/female): ")).lower()
 hemoglobin_value = float(input("Enter hemoglobin value (g/l): "))
 if biological_gender == "female":
-    if hemoglobin_value >= 117 and hemoglobin_value <= 155:
+    if hemoglobin_value >= 117 and hemoglobin_value <= 155 :
         print("Your hemoglobin is normal.")
     elif hemoglobin_value < 117 :
         print("Your hemoglobin is low.")
     elif hemoglobin_value > 155:
         print("Your hemoglobin is high.")
 elif biological_gender == "male":
-    if hemoglobin_value >= 134 and hemoglobin_value <= 167:
+    if hemoglobin_value >= 134 and hemoglobin_value <= 167 :
         print("Your hemoglobin is normal.")
     elif hemoglobin_value < 134:
         print("Your hemoglobin is low.")
