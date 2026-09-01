5b99fa0fa11cf0aa690bd9038c18371761b55172
Bael Haaken <hoangphale12345@gmail.com>
2026-09-01 13:22:25 +0300

    project player's name

---
+++ project/players_name.py
@@ -0,0 +1,6 @@
+player_name = input("Enter your name: ")
+player_age = int(input("Enter your age: "))
+
+print(f"\nWelcome, {player_name}!")
+print(f"You are {player_age} years old.")
+print("WELCOME TO KING'S LANDING")
---
+++ project/readme.md
@@ -0,0 +1,2 @@
+## King's Landing
+Nguyen Ba Viet Hoang
