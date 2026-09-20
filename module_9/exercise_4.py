4b9c5d95b2be6153de552432c64dfe0860a6975a
Bael Haaken <hoangphale12345@gmail.com>
2026-09-20 19:31:17 +0300

    mod 9

---
+++ module_9/exercise_4.py
@@ -0,0 +1,33 @@
+import random
+
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+        
+    def accelerate(self, change):
+        self.current_speed += change
+
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+
+        if self.current_speed < 0:
+            self.current_speed = 0
+    
+    def drive(self, hour):
+        self.travelled_distance += self.current_speed * hour
+
+def race(cars):
+    while True:
+        for car in cars:
+            change = random.randint(-10, 15)
+            car.accelerate(change)
+
+        for car in cars:
+            car.drive(1)
+
+        for car in cars:
+            if car.travelled_distance >= 10000:
+                return cars
